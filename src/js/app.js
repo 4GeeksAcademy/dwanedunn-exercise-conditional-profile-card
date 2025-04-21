b9a31bd767689e87f65f4fb616443cf7853aeba8
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-21 00:02:17 +0000

    revision updates socialMediaPosition

--- src/js/app.js
+++ src/js/app.js
@@ -40,7 +40,7 @@ function render(variables = {}) {
           <h3>${variables.city === null ? "Miami" : variables.city}, ${
     variables.country === null ? "USA" : variables.country
   }</h3>
-          <ul class="position-right">
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/${
               variables.twitter === null ? "4geeksacademy" : variables.twitter
             }"><i class="fab fa-twitter"></i></a></li>
